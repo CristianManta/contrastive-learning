130ae831b87ca22f07ef8ffbfa40a3e6ad919cb2
Cristian <dragos.manta@yahoo.ca>
2020-08-05 12:38:57 -0400

    Restructured the repo a bit for the imagenet training. Tikhonov code is WIP

--- regularization/train.py
+++ regularization/train.py
@@ -1,6 +1,16 @@
+import argparse
+import os, shutil, sys, pathlib
+
+_pth = str(pathlib.Path(__file__).absolute())
+for i in range(2):
+    (_pth, _) = os.path.split(_pth)
+sys.path.insert(0, _pth)  # I just made sure that the root of the project (ContrastiveTeamO) is in the path where Python
+# looks for packages in order to import from files that require going several levels up from the directory where this
+# script is. Unfortunately, by default Python doesn't allow imports from above the current file directory.
+
+
 import random
 import time, datetime
-import os, shutil
 import yaml
 import ast, bisect
 import csv
@@ -8,22 +18,76 @@ import csv
 import numpy as np
 import torch
 import torch.nn as nn
+from torch.utils.data.sampler import SubsetRandomSampler
+from torch.utils.data import DataLoader
+
+import torchvision
+import torchvision.transforms as transforms
+from torchvision.datasets import CIFAR10
+
 import torch.backends.cudnn as cudnn
 from torch import optim
 from torch.optim.lr_scheduler import LambdaLR
 from torch.autograd import grad
 import torchnet as tnt
 
-import dataloader
-from dataloader import cutout
-import models
+from custom_transforms import get_color_distortion
+import models.cifar as cifarmodels
+from loss.nt_xent import NTXentLoss
 
-# -------------
-# Initial setup
-# -------------
+parser = argparse.ArgumentParser('constructive learning training on CIFAR-10')
+parser.add_argument('--data-dir', type=str, default='/home/math/oberman-lab/data/', metavar='DIR',
+                    help='Directory where CIFAR-10 data is saved')
+parser.add_argument('--seed', type=int, default=0, metavar='S',
+                    help='random seed (default: 0)')
+parser.add_argument('--epochs', type=int, default=100, metavar='N',
+                    help='number of epochs to train (default: 100)')
+parser.add_argument('--test-batch-size', type=int, default=128, metavar='N',
+                    help='input batch size for testing (default: 128)')
+# parser.add_argument('--log-interval', type=int, default=100, metavar='N',
+#        help='how many batches to wait before logging training status (default: 100)')
+# parser.add_argument('--logdir', type=str, default=None,metavar='DIR',
+#        help='directory for outputting log files. (default: ./logs/DATASET/MODEL/TIMESTAMP/)')
 
-# Parse command line arguments
-from argparser import parser
+group1 = parser.add_argument_group('Model hyperparameters')
+group1.add_argument('--model', type=str, default='ResNet50',
+                    help='Model architecture (default: ResNet50)')
+group1.add_argument('--dropout', type=float, default=0, metavar='P',
+                    help='Dropout probability, if model supports dropout (default: 0)')
+group1.add_argument('--bn', action='store_true', dest='bn',
+                    help="Use batch norm")
+group1.add_argument('--no-bn', action='store_false', dest='bn',
+                    help="Don't use batch norm")
+group1.set_defaults(bn=True)
+group1.add_argument('--last-layer-nonlinear',
+                    action='store_true', default=False)
+group1.add_argument('--bias', action='store_true', dest='bias',
+                    help="Use model biases")
+group1.add_argument('--no-bias', action='store_false', dest='bias',
+                    help="Don't use biases")
+group1.set_defaults(bias=False)
+group1.add_argument('--kernel-size', type=int, default=3, metavar='K',
+                    help='convolution kernel size (default: 3)')
+group1.add_argument('--model-args', type=str,
+                    default="{}", metavar='ARGS',
+                    help='A dictionary of extra arguments passed to the model.'
+                         ' (default: "{}")')
+
+group0 = parser.add_argument_group('Optimizer hyperparameters')
+group0.add_argument('--batch-size', type=int, default=128, metavar='N',
+                    help='Input batch size for training. (default: 128)')
+group0.add_argument('--lr', type=float, default=3e-4, metavar='LR',
+                    help='Initial step size. (default: 0.15)')
+group0.add_argument('--lr-schedule', type=str, metavar='[[epoch,ratio]]',
+                    default='[[0,1],[30,0.2],[60,0.04],[80,0.008]]', help='List of epochs and multiplier '
+                                                                          'for changing the learning rate (default: [[0,1],[30,0.2],[60,0.04],[80,0.008]]). ')
+group0.add_argument('--momentum', type=float, default=0.9, metavar='M',
+                    help='SGD momentum parameter (default: 0.9)')
+
+group2 = parser.add_argument_group('Regularizers')
+group2.add_argument('--decay', type=float, default=1e-5, metavar='L',
+                    help='Lagrange multiplier for weight decay (sum '
+                         'parameters squared) (default: 1e-6)')
 
 args = parser.parse_args()
 
@@ -32,74 +96,89 @@ has_cuda = torch.cuda.is_available()
 cudnn.benchmark = True
 
 # Set random seed
-if args.seed is None:
-    args.seed = int(time.time())
 torch.manual_seed(args.seed)
-random.seed(args.seed)
 np.random.seed(args.seed)
 
-# Set and create logging directory
-if args.logdir is None:
-    args.logdir = os.path.join('./logs/', 'regularized', args.model,
-                               '{0:%Y-%m-%dT%H%M%S}'.format(datetime.datetime.now()))
-os.makedirs(args.logdir, exist_ok=True)
-
-# Print arguments to std out
-# and save argument values to yaml file
-print('Arguments:')
+# Print args
+print('Contrastive Learning on Cifar-10')
 for p in vars(args).items():
     print('  ', p[0] + ': ', p[1])
 print('\n')
 
-args_file_path = os.path.join(args.logdir, 'args.yaml')
-with open(args_file_path, 'w') as f:
-    yaml.dump(vars(args), f, default_flow_style=False)
+# Set and create logging directory
+# if args.logdir is None:
+#    args.logdir = os.path.join('./logs/',args.dataset,args.model,
+#            '{0:%Y-%m-%dT%H%M%S}'.format(datetime.datetime.now()))
+# os.makedirs(args.logdir, exist_ok=True)
 
-workers = 4
-test_loader = getattr(dataloader, 'cifar10')(args.datadir,
-                                             mode='test', transform=False,
-                                             batch_size=args.test_batch_size,
-                                             num_workers=workers,
-                                             shuffle=False,
-                                             pin_memory=has_cuda)
+# Get Train and Test Loaders
+# Do 3 deparate train loaders, one with each data augmentation
+root = os.path.join(args.data_dir, 'cifar10')
 
-transforms = [cutout(args.cutout, channels=3)]
-train_loader = getattr(dataloader, 'cifar10')(args.datadir,
-                                              mode='train', transform=True,
-                                              batch_size=args.batch_size,
-                                              training_transforms=transforms,
-                                              num_workers=workers,
-                                              shuffle=True,
-                                              pin_memory=has_cuda,
-                                              drop_last=True)
 
-model = getattr(models, args.model)()
+class SimCLRDataTransform(object):
+    def __init__(self, transform):
+        self.transform = transform
 
-criterion = nn.CrossEntropyLoss()
-train_criterion = nn.CrossEntropyLoss(reduction='none')
+    def __call__(self, sample):
+        xi = self.transform(sample)
+        xj = self.transform(sample)
+        return xi, xj
+
+
+color_jitter = transforms.ColorJitter(0.8, 0.8, 0.8, 0.2)
+
+data_transforms = transforms.Compose([transforms.RandomResizedCrop(size=32),
+                                      transforms.RandomHorizontalFlip(),
+                                      get_color_distortion(s=1.0),
+                                      transforms.ToTensor()])
+
+data_augment = data_transforms
+ds_train = CIFAR10(root, download=True, train=True, transform=SimCLRDataTransform(data_augment))
+
+num_train = len(ds_train)
+indices = list(range(num_train))
+np.random.shuffle(indices)
+
+valid_size = 0.05
+split = int(np.floor(valid_size * num_train))
+train_idx, valid_idx = indices[split:], indices[:split]
+
+train_sampler = SubsetRandomSampler(train_idx)
+valid_sampler = SubsetRandomSampler(valid_idx)
+
+train_loader = DataLoader(ds_train, batch_size=args.batch_size, sampler=train_sampler,
+                          num_workers=4, drop_last=True, shuffle=False)
+valid_loader = DataLoader(ds_train, batch_size=args.test_batch_size, sampler=valid_sampler,
+                          num_workers=4, drop_last=True)
+
+# initialize model and move it the GPU (if available)
+classes = 10
+model_args = ast.literal_eval(args.model_args)
+in_channels = 3
+model_args.update(bn=args.bn, classes=classes, bias=args.bias,
+                  kernel_size=args.kernel_size,
+                  in_channels=in_channels,
+                  softmax=False, last_layer_nonlinear=args.last_layer_nonlinear,
+                  dropout=args.dropout)
+model = getattr(cifarmodels, args.model)(**model_args)
 
 if has_cuda:
-    criterion = criterion.cuda(0)
-    train_criterion = train_criterion.cuda(0)
-    model = model.cuda(0)
+    model = model.cuda()
+    if torch.cuda.device_count() > 1:
+        model = nn.DataParallel(model)
 
-optimizer = optim.SGD(model.parameters(),
-                      lr=args.lr,
-                      weight_decay=args.decay,
-                      momentum=args.momentum,
-                      nesterov=False)
+# print(model)
 
+# Set Optimizer and learning rate schedule
+optimizer = torch.optim.Adam(model.parameters(), args.lr, weight_decay=args.decay)
+scheduler = torch.optim.lr_scheduler.CosineAnnealingLR(optimizer, T_max=len(train_loader), eta_min=0,
+                                                       last_epoch=-1)
+# scheduler = torch.optim.lr_scheduler.StepLR(optimizer, 20, gamma=0.1, last_epoch=-1)
 
-def scheduler(optimizer, lr_schedule):
-    """Return a hyperparmeter scheduler for the optimizer"""
-    lS = np.array(ast.literal_eval(lr_schedule))
-    llam = lambda e: float(lS[max(bisect.bisect_right(lS[:, 0], e) - 1, 0), 1])
-    lscheduler = LambdaLR(optimizer, llam)
-
-    return lscheduler
-
-
-schedule = scheduler(optimizer, args.lr_schedule)
+# define loss
+nt_xent_criterion = NTXentLoss(device=torch.cuda.current_device(), batch_size=args.batch_size, temperature=0.5,
+                               use_cosine_similarity=True)
 
 # --------
 # Training
